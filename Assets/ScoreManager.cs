b353e11be9e12a1a19698e4166c5bfe47462995c
francisco-eduqo <francisco@eduqo.com.br>
2020-10-05 18:54:47 -0300

    Implementing score text

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class ScoreManager : MonoBehaviour
 
     public float maxHitScore = 50f;
     public float timeLimit = 1f;
+
+    public Text scoreText;
 
     private void Awake()
     {
@@ -26,11 +29,17 @@ public class ScoreManager : MonoBehaviour
     private void Start()
     {
         score = 0f;
+        UpdateScoreText(score);
     }
 
     public void CountScore(float hitTime)
     {
         float hitScore = (-(1 / Mathf.Pow(timeLimit, 2)) * Mathf.Pow(hitTime, 2) + 1) * maxHitScore;
         score += hitScore;
+        UpdateScoreText(score);
+    }
+
+    private void UpdateScoreText(float score) {
+        scoreText.text = "Score: " + ((int)Mathf.Round(score)).ToString();
     }
 }
